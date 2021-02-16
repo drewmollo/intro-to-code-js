4222b26354f3ccb87e85937491f1365a87e048c7
Christian Mejia <chrismejia10@gmail.com>
2021-02-15 23:31:04 -0500

    feat(03-Answers): add numbering; descriptions, examples; challenges; all test code passing

--- 03-Methods-and-Functions/index.js
+++ 03-Methods-and-Functions/index.js
@@ -4,7 +4,7 @@
  * Define the function helloWorld.
  * helloWorld takes no inputs and returns the phrase "Hello World!"
  *
- * EXAMPLE FUNCTION CALL:
+ * EXAMPLE:
  * helloWorld() => "Hello World!"
  */
 
@@ -17,18 +17,15 @@ function helloWorld() {
  *
  * Define the function helloWorldRedux.
  * helloWorldRedux takes in an optional input, name.
- * If a name value is passed, return a personalized greeting using the name.
+ * When a name value is passed, return a personalized greeting using the name.
  * Otherwise, return the phrase "Hello World!"
  *
- * EXAMPLE FUNCTION CALLS:
+ * EXAMPLES:
  * helloWorldRedux() => "Hello World!"
  * helloWorldRedux("Bob") => "Hello Bob!"
  *
  * CHALLENGE:
- * Can you write this function as a one-liner?
- *
- * CHALLENGE NOTE:
- * Comment out the original function before testing your one-line version to avoid duplicate declaration errors!
+ * Can you write this function as a one-liner? Remember to comment out the original function before testing your one-line version to avoid duplicate declaration errors!
  */
 
 function helloWorldRedux(name) {
@@ -51,11 +48,14 @@ function helloWorldRedux(name) {
  * READ:
  * .toUpperCase() | https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
  *
- * CHALLENGE:
- * Can you write this function as a one-liner?
+ * EXAMPLES:
+ * uppercaseThis("aaaa") => "AAAA"
+ * uppercaseThis("BBBB") => "BBBB"
+ * uppercaseThis("cCcC") => "CCCC"
+ * uppercaseThis("d1D2d3") => "D1D2D3"
  *
- * CHALLENGE NOTE:
- * Comment out the original function before testing your one-line version to avoid duplicate declaration errors!
+ * CHALLENGE:
+ * Can you write this function as a one-liner? Remember to comment out the original function before testing your one-line version to avoid duplicate declaration errors!
  */
 
 function uppercaseThis(phrase) {
@@ -66,37 +66,19 @@ function uppercaseThis(phrase) {
 // const uppercaseThis = phrase => phrase.toUpperCase()
 
 /**********************************
- * #: arrayToString
+ * #4: doesItAddUp
  *
- * arrayToString(["cat", "dog", "moo"], "") => "catdogmoo"
- * arrayToString(["cat", "dog", "moo"], " ") => "cat dog moo"
- * arrayToString(["cat", "dog", "moo"], "+-%") => "cat+-%dog+-%moo"
+ * Define the function doesItAddUp.
+ * doesItAddUp accepts three number inputs.
+ * doesItAddUp returns true when the first two inputs add up to the third.
+ * Otherwise, doesItAddUp returns false.
  *
- * CHALLENGE:
- * Can you write this function as a one-liner?
- *
- * CHALLENGE NOTE:
- * Comment out the original function before testing your one-line version to avoid duplicate declaration errors!
- */
-
-function arrayToString(array, separator) {
-  return array.join(separator);
-}
-
-// One-line answer
-// const arrayToString = (string, separator) => array.join(separator)
-
-/**********************************
- * #: doesItAddUp
- *
+ * EXAMPLES:
  * doesItAddUp(10, 5, 15) => true
  * doesItAddUp(10, 5, 20) => false
  *
  * CHALLENGE:
- * Can you write this function as a one-liner?
- *
- * CHALLENGE NOTE:
- * Comment out the original function before testing your one-line version to avoid duplicate declaration errors!
+ * Can you write this function as a one-liner? Remember to comment out the original function before testing your one-line version to avoid duplicate declaration errors!
  */
 
 function doesItAddUp(numA, numB, numC) {
@@ -110,23 +92,48 @@ function doesItAddUp(numA, numB, numC) {
 // const doesItAddUp = (numA, numB, numC) => numA + numB === numC;
 
 /**********************************
- * #: smallTogetherNow
+ * #5: arrayToString
+ *
+ * Define the function arrayToString.
+ * arrayToString accepts an array containing any number of strings of any length, and a connector, a string made up of any number of random characters.
+ * arrayToString returns a combined string made up of each string in the array attached together by the connector.
+ *
+ * EXAMPLES:
+ * arrayToString(["cat", "dog", "moo"], "") => "catdogmoo"
+ * arrayToString(["cat", "dog", "moo"], " ") => "cat dog moo"
+ * arrayToString(["cat", "dog", "moo"], "+-%") => "cat+-%dog+-%moo"
+ *
+ * CHALLENGE:
+ * Can you write this function as a one-liner? Remember to comment out the original function before testing your one-line version to avoid duplicate declaration errors!
+ */
+
+function arrayToString(array, connector) {
+  return array.join(connector);
+}
+
+// One-line answer
+// const arrayToString = (string, separator) => array.join(separator)
+
+/**********************************
+ * #6: smallTogetherNow
  *
  * Define the function smallTogetherNow.
  * smallTogetherNow accepts two strings of any length.
- * smallTogetherNow returns a string that is the combination of both input strings converted to all lowercase.
+ * smallTogetherNow returns a single string, the combination of both input strings converted to all lowercase.
  *
  * READ:
  * .toLowerCase() | https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toLowerCase
  *
+ * EXAMPLES:
+ * smallTogetherNow("HELLO", "WORLD") => "helloworld"
+ * smallTogetherNow("apple", pie) => "applepie"
+ * smallTogetherNow("mIxEd", "CaPiTaLs") => "mixedcapitals"
+ *
  * CHALLENGE:
- * Can you write this function as a one-liner?
+ * Can you write this function as a one-liner? Remember to comment out the original function before testing your one-line version to avoid duplicate declaration errors!
  *
  * CHALLENGE HINT:
  * .concat() | https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/concat
- *
- * CHALLENGE NOTE:
- * Comment out the original function before testing your one-line version to avoid duplicate declaration errors!
  */
 
 function smallTogetherNow(str1, str2) {
@@ -141,24 +148,24 @@ function smallTogetherNow(str1, str2) {
 //   str1.toLowerCase().concat(str2.toLowerCase());
 
 /**********************************
- * #: Dog owners and their dogs
+ * #7: Dog owners and their dogs
  *
  * Define the function dogAndOwnerInfo.
  * dogAndOwnerInfo takes in four inputs:
- * - the dog owner's name
- * - the dog owner's age
  * - the dog's name
  * - the dog's age
+ * - the dog owner's name
+ * - the dog owner's age
  *
- * dogAndOwnerInfo converts the dog's age into human years by multiplying it by 7.
+ * dogAndOwnerInfo converts the dog's age to human years by multiplying it by 7.
  *
- * dogAndOwnerInfo returns one of three phrases with the corresponding values:
- * - if the dog is older than their owner:
- *    "[dog's name] is older than their owner, [owner's name], by [difference in human years]."
- * - if the owner is older than their owner:
- *    "[owner's name] is older than their dog, [dog's name], by [difference in human years]."
- * - if the owner and their dog are the same age:
- *    "[owner's name] and [dog's name] are both [their age in human years] old."
+ * EXAMPLES:
+ * dogAndOwnerInfo("Turbo", 10, "Richard", 55) =>
+ *    "Turbo is older than their owner, Richard, by 15 years."
+ * dogAndOwnerInfo("Fido", 1, "Sara", 15) =>
+ *    "Sara is older than their dog, Fido, by 8 years."
+ * dogAndOwnerInfo("Star", 2, "John", 14) =>
+ *    "John and Star are the both 14 years old."
  */
 
 function dogAndOwnerInfo(dogName, dogAge, ownerName, ownerAge) {
@@ -178,19 +185,26 @@ function dogAndOwnerInfo(dogName, dogAge, ownerName, ownerAge) {
 }
 
 /**********************************
- * BONUS
- * #1: Many inputs, if-else, direct usage and return
- *  animal years
- */
-
-/**********************************
- * #1: One input, multiple ifs, local variable, final if-return
- * isCompatible (dating)
- */
-
-/**********************************
- * #1: three inputs, multiple checks
+ * #8: doesTheMathWork
  *
+ * Define the function doesTheMathWork.
+ * doesTheMathWork accept three inputs, each a number.
+ * doesTheMathWork returns a phrase based on the following:
+ * - first input + second input = third input -> "addition"
+ * - first input - second input = third input -> "subtraction"
+ * - first input * second input = third input -> "multiplication"
+ * - first input / second input = third input -> "division"
+ * - none of the above -> "no operation"
+ *
+ * NOTE:
+ * Each set of three inputs resolves to only ONE of the given possible outputs.
+ *
+ * EXAMPLES:
+ * doesTheMathWork(1, 2, 3) => "addition"
+ * doesTheMathWork(7, 2, 5) => "subtraction"
+ * doesTheMathWork(3, 3, 9) => "multiplication"
+ * doesTheMathWork(81, 9, 9) => "division"
+ * doesTheMathWork(11, 222, 3) = "no operation"
  */
 
 function doesTheMathWork(numA, numB, numC) {
@@ -208,10 +222,26 @@ function doesTheMathWork(numA, numB, numC) {
 }
 
 /**********************************
- * BONUS
+ * #9: allWordsLength
  *
- * allWordsLength
+ * Define the function allWordsLength.
+ * allWordsLength accepts an array containing any number of strings, each of any length.
+ * allWordsLength returns the total length of all the strings in the array.
  *
+ * HINT: If you're familiar with loops, you might be tempted to try using one here. You can solve this with a loop, but it's not the most direct solution. Think about how you can go from your starting data type to the desired output data type. What built-in methods can you use here to help out?
+ *
+ * READ:
+ * Array Methods | https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+ * String Methods | https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+ *
+ * EXAMPLES:
+ * allWordsLength(["apple"]) => 5
+ * allWordsLength(["old", "textbook"]) => 11  (3 + 8)
+ * allWordsLength(["cat", "dog", "moo"]) => 9 (3 + 3 + 3)
+ * allWordsLength(["robin", "hood", "men", "in", "tights"]) => 19 (5 + 4 + 3 + 2 + 5)
+ *
+ * CHALLENGE:
+ * Can you write this function as a one-liner? Remember to comment out the original function before testing your one-line version to avoid duplicate declaration errors!
  */
 
 function allWordsLength(array) {
@@ -248,15 +278,39 @@ describe("03 - Methods and Functions", () => {
   });
 
   describe("#3: uppercaseThis", () => {
-    it("returns the phrase completely capitalized", () => {
-      expect(uppercaseThis("aaaa")).to.equal("AAAA");
-      expect(uppercaseThis("BBBB")).to.equal("BBBB");
-      expect(uppercaseThis("cCcC")).to.equal("CCCC");
+    describe("returns the phrase completely capitalized", () => {
+      it("when the phrase is all lowercase", () => {
+        expect(uppercaseThis("aaaa")).to.equal("AAAA");
+      });
+
+      it("when the phrase is already all capitalized", () => {
+        expect(uppercaseThis("BBBB")).to.equal("BBBB");
+      });
+
+      it("when the phrase has mixed capitals", () => {
+        expect(uppercaseThis("cCcC")).to.equal("CCCC");
+      });
       expect(uppercaseThis("d1D2d3")).to.equal("D1D2D3");
     });
   });
 
-  describe("#: arrayToString", () => {
+  describe("#4: doesItAddUp", () => {
+    it("returns true -> first two inputs equal the third", () => {
+      expect(doesItAddUp(1, 2, 3)).to.be.true;
+      expect(doesItAddUp(2, 1, 3)).to.be.true;
+      expect(doesItAddUp(4, 4, 8)).to.be.true;
+      expect(doesItAddUp(10, 20, 30)).to.be.true;
+    });
+
+    it("returns false -> first two inputs don't equal the third", () => {
+      expect(doesItAddUp(1, 5, 3)).to.be.false;
+      expect(doesItAddUp(22, 64, 33)).to.be.false;
+      expect(doesItAddUp(1, 1, 1)).to.be.false;
+      expect(doesItAddUp(1, 9, 2)).to.be.false;
+    });
+  });
+
+  describe("#5: arrayToString", () => {
     const array = ["cat", "dog", "moo"];
     describe("returns the correct string", () => {
       it("when the separator is an empty string ('')", () => {
@@ -273,23 +327,7 @@ describe("03 - Methods and Functions", () => {
     });
   });
 
-  describe("#: doesItAddUp", () => {
-    it("returns true -> first two inputs equal the third", () => {
-      expect(doesItAddUp(1, 2, 3)).to.be.true;
-      expect(doesItAddUp(2, 1, 3)).to.be.true;
-      expect(doesItAddUp(4, 4, 8)).to.be.true;
-      expect(doesItAddUp(10, 20, 30)).to.be.true;
-    });
-
-    it("returns false -> first two inputs don't equal the third", () => {
-      expect(doesItAddUp(1, 5, 3)).to.be.false;
-      expect(doesItAddUp(22, 64, 33)).to.be.false;
-      expect(doesItAddUp(1, 1, 1)).to.be.false;
-      expect(doesItAddUp(1, 9, 2)).to.be.false;
-    });
-  });
-
-  describe("#: smallTogetherNow", () => {
+  describe("#6: smallTogetherNow", () => {
     describe("returns a unified, lowercase string", () => {
       it("when two uppercase input strings are passed in", () => {
         expect(smallTogetherNow("HELLO", "WORLD")).to.equal("helloworld");
@@ -305,7 +343,7 @@ describe("03 - Methods and Functions", () => {
     });
   });
 
-  describe("#: Dog owners and their dogs", () => {
+  describe("#7: Dog owners and their dogs", () => {
     describe("returns the correct phrase", () => {
       it("when dog is older than their owner", () => {
         expect(dogAndOwnerInfo("Turbo", 10, "Richard", 55)).to.equal(
@@ -314,8 +352,8 @@ describe("03 - Methods and Functions", () => {
       });
 
       it("when owner is older than their dog", () => {
-        expect(dogAndOwnerInfo("Fido", 1, "Michael", 15)).to.equal(
-          "Michael is older than their dog, Fido, by 8 years."
+        expect(dogAndOwnerInfo("Fido", 1, "Sara", 15)).to.equal(
+          "Sara is older than their dog, Fido, by 8 years."
         );
       });
 
@@ -327,10 +365,56 @@ describe("03 - Methods and Functions", () => {
     });
   });
 
-  describe("#: doesTheMathWork", () => {
+  describe("#8: doesTheMathWork", () => {
     describe("returns the correct operation", () => {
-      it("when ", () => {});
-      it("", () => {});
+      it("first two inputs add (+) to third input -> 'addition'", () => {
+        expect(doesTheMathWork(1, 2, 3)).to.equal("addition");
+        expect(doesTheMathWork(5, 2, 7)).to.equal("addition");
+        expect(doesTheMathWork(10, 2, 12)).to.equal("addition");
+      });
+
+      it("first two inputs subtract (-) to third input -> 'subtraction'", () => {
+        expect(doesTheMathWork(3, 2, 1)).to.equal("subtraction");
+        expect(doesTheMathWork(7, 2, 5)).to.equal("subtraction");
+        expect(doesTheMathWork(12, 2, 10)).to.equal("subtraction");
+      });
+
+      it("first two inputs multiply (*) to third input -> 'multiplication'", () => {
+        expect(doesTheMathWork(3, 3, 9)).to.equal("multiplication");
+        expect(doesTheMathWork(15, 5, 75)).to.equal("multiplication");
+        expect(doesTheMathWork(12, 2, 24)).to.equal("multiplication");
+      });
+
+      it("first two inputs divide (/) to third input -> 'division'", () => {
+        expect(doesTheMathWork(50, 2, 25)).to.equal("division");
+        expect(doesTheMathWork(81, 9, 9)).to.equal("division");
+        expect(doesTheMathWork(48, 8, 6)).to.equal("division");
+      });
+
+      it("first two inputs don't resolve to third input -> 'no operation'", () => {
+        expect(doesTheMathWork(1, 99, 72)).to.equal("no operation");
+        expect(doesTheMathWork(5, 84, 7)).to.equal("no operation");
+        expect(doesTheMathWork(11, 222, 333)).to.equal("no operation");
+      });
+    });
+  });
+
+  describe("#9: allWordsLength", () => {
+    const testWords = [
+      ["apple"],
+      ["old", "textbook"],
+      ["cat", "dog", "moo"],
+      ["robin", "hood", "men", "in", "tights"],
+    ];
+
+    describe("returns the combined length of all strings in the array", () => {
+      testWords.forEach((array) => {
+        const func = (a) => a.join("").length;
+        const result = func(array);
+        it(`${JSON.stringify(array)} -> ${result}`, () => {
+          expect(allWordsLength(array)).to.equal(result);
+        });
+      });
     });
   });
 });
